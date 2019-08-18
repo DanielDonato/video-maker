0216be7985b9fd6f607e4e11876e3c4d375984f5
DanielDonato <daniel_donato10@yahoo.com.br>
2019-08-18 19:49:18 -0300

    Fix

--- robots/text.js
+++ robots/text.js
@@ -1,53 +1,59 @@
-const algorithmia = require('algorithmia');
-const algorithmiaAPiKey = require("../credentials/algorithmia.json").apiKey;
-const sentenceBoundaryDetection = require('sbd');
+const algorithmia = require('algorithmia')
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+const sentenceBoundaryDetection = require('sbd')
 
-async function robot(content){
-    await fetchContentFromWikipedia(content);
-    sanitizeContent(content);
-    breackContentIntoSentences(content);
+async function robot(content) {
+  await fetchContentFromWikipedia(content)
+  sanitizeContent(content)
+  breakContentIntoSentences(content)
 
-    async function fetchContentFromWikipedia(content){
-        const algorithmiaAuthenticated = algorithmia(algorithmiaAPiKey);
-        const wikipediaAlgorithm = algorithmiaAuthenticated.algo("web/WikipediaParser/0.1.2?timeout=300");
-        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.shearchTearm);
-        const wikipediaContent = wikipediaResponse.get();
-        content.sourceContentOriginal = wikipediaContent.content;
-    }
+  async function fetchContentFromWikipedia(content) {
+    const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+    const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+    const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
+    const wikipediaContent = wikipediaResponde.get()
 
-    function sanitizeContent(content){
-        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkDown(content.sourceContentOriginal);
-        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown);
-        
-        content.sourceContentSanitized = withoutDatesInParentheses;
+    content.sourceContentOriginal = wikipediaContent.content
+  }
 
-        function removeBlankLinesAndMarkDown(text) {
-            const allLines = text.split('\n');
-            const withoutBlankLinesAndMarkdown = allLines.filter(line => {
-                if(line.trim().length === 0 || line.trim().startsWith('=')){
-                    return false
-                }
-                return true;
-            });
-            return withoutBlankLinesAndMarkdown.join(' ');
+  function sanitizeContent(content) {
+    const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+    const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+    content.sourceContentSanitized = withoutDatesInParentheses
+
+    function removeBlankLinesAndMarkdown(text) {
+      const allLines = text.split('\n')
+
+      const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+        if (line.trim().length === 0 || line.trim().startsWith('=')) {
+          return false
         }
 
-        function removeDatesInParentheses(text){
-            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ');
-        }
-    }
+        return true
+      })
 
-    function breackContentIntoSentences(content){
-        content.sentences = [];
-        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized);
-        sentences.forEach((sentence) => {
-            content.sentences.push({
-                text: sentence,
-                keywords: [],
-                images: []
-            });
-        })
+      return withoutBlankLinesAndMarkdown.join(' ')
     }
+  }
+
+  function removeDatesInParentheses(text) {
+    return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+  }
+
+  function breakContentIntoSentences(content) {
+    content.sentences = []
+
+    const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+    sentences.forEach((sentence) => {
+      content.sentences.push({
+        text: sentence,
+        keywords: [],
+        images: []
+      })
+    })
+  }
 
 }
-module.exports = robot;
+
+module.exports = robot
